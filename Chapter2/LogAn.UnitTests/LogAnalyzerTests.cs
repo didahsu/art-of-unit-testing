16cffc67a419b0849ea0c533423d0c1f6c8f074f
Dida Hsu <didahsu@gmail.com>
2020-12-29 17:55:57 +0800

    chapter 2 - use nunit to writing first test

---
+++ Chapter2/LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidFileName_BadExtension_ReturnsFalse()
+        {
+            var analyzer = new LogAnalyzer();
+            var result = analyzer.IsValidLogFileName("filewithbadextension.form");
+            Assert.That(result, Is.False);
+        }
+    }
+}
