57895e352a6ed2c2cc676f7514bbdcff717212f6
許嘉仁 <dida_hsu@mayohr.com>
2020-12-29 18:00:59 +0800

    chapter 2 - red -> green -> refactor

--- Chapter2/LogAn.UnitTests/LogAnalyzerTests.cs
+++ Chapter2/LogAn.UnitTests/LogAnalyzerTests.cs
@@ -12,5 +12,21 @@ namespace LogAn.UnitTests
             var result = analyzer.IsValidLogFileName("filewithbadextension.form");
             Assert.That(result, Is.False);
         }
+        
+        [Test]
+        public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue()
+        {
+            var analyzer = new LogAnalyzer();
+            var result = analyzer.IsValidLogFileName("filewithbadextension.slf");
+            Assert.That(result, Is.True);
+        }
+        
+        [Test]
+        public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue()
+        {
+            var analyzer = new LogAnalyzer();
+            var result = analyzer.IsValidLogFileName("filewithbadextension.SLF");
+            Assert.That(result, Is.True);
+        }
     }
 }
